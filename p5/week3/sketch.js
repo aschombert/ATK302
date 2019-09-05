e996dc4a7104747121862362146353c4cd304df6
Abi Schombert <aeschom@ilstu.edu>
2019-09-05 09:21:44 -0500

    im really trying here man

---
+++ p5/week3/sketch.js
@@ -0,0 +1,15 @@
+var score = 0;
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  // put drawing code here
+}
+
+function mouseReleased(){
+  ellipse( mouseX, mouseY, 20, 20);
+}
